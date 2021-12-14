effa6075578d1652cd3f4910417695c67fa9d2d7
Matt Raible <matt.raible@okta.com>
2021-12-13 17:26:40 -0700

    Example with WebFlux that works with Spring Native

--- src/main/java/com/example/sample/Application.java
+++ src/main/java/com/example/sample/Application.java
@@ -2,11 +2,17 @@ package com.example.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.TypeHint;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@TypeHint(types = {
+    // remove this class and login will fail after you're redirected back to the app
+    JwtDecoder.class
+})
 @SpringBootApplication
 public class Application {
 
---
+++ src/main/java/com/example/sample/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.example.sample;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+            .authorizeExchange(ae -> ae.anyExchange().authenticated())
+            .oauth2Login(Customizer.withDefaults())
+            .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt)
+            .oauth2Client(Customizer.withDefaults())
+            .build();
+    }
+
+    @Bean
+    ReactiveJwtDecoder reactiveJwtDecoder(@Value("${spring.security.oauth2.client.provider.okta.issuer-uri}") String issuerUri) {
+        return ReactiveJwtDecoders.fromOidcIssuerLocation(issuerUri);
+    }
+}
