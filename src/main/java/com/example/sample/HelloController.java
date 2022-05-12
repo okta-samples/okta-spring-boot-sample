15d1d2cd2c2def1f1534fa36812b1e271cab902e
Matt Raible <matt.raible@okta.com>
2022-05-11 20:28:35 -0600

    Add hello API and documentation (#37)

---
+++ src/main/java/com/example/sample/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Principal principal) {
+        return "Hello, " + principal.getName() + "!";
+    }
+}
