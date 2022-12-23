3685243520e6a09e07353192d4ed0bc17c20228d
Brian Demers <bdemers@apache.org>
2022-12-23 14:32:06 -0500

    Updates example to use Spring MVC + Thymeleaf
    Code copied from https://github.com/okta/samples-java-spring/tree/master/okta-hosted-login and updated to use Bootstrap 5

--- src/main/java/com/example/sample/Application.java
+++ src/main/java/com/example/sample/Application.java
@@ -2,24 +2,80 @@ package com.example.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
+import java.util.Collections;
 
 @SpringBootApplication
+@EnableMethodSecurity(securedEnabled = true)
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @RestController
-    static class SimpleRestController {
+    /**
+     * The default Spring logout behavior redirects a user back to {code}/login?logout{code}, so you will likely want
+     * to change that.  The easiest way to do this is by creating a {@link SecurityFilterChain}.
+     */
+    @Configuration
+    static class SecurityConfig {
+        @Bean
+        protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+            return http.authorizeHttpRequests()
+                    // allow antonymous access to the root page
+                    .antMatchers("/").permitAll()
+                    // all other requests
+                    .anyRequest().authenticated()
+
+                // set logout URL
+                .and().logout().logoutSuccessUrl("/")
+
+                // enable OAuth2/OIDC
+                .and().oauth2Login()
+                .and().oauth2ResourceServer().jwt()
+                .and().and().build();
+        }
+    }
+
+    /**
+     * This example controller has endpoints for displaying the user profile info on {code}/{code} and "you have been
+     * logged out page" on {code}/post-logout{code}.
+     */
+    @Controller
+    static class ExampleController {
+
         @GetMapping("/")
+        String home() {
+            return "home";
+        }
+
+        @GetMapping("/profile")
+        @PreAuthorize("hasAuthority('SCOPE_profile')")
+        ModelAndView userDetails(OAuth2AuthenticationToken authentication) {
+            return new ModelAndView("userProfile" , Collections.singletonMap("details", authentication.getPrincipal().getAttributes()));
+        }
+    }
+
+    @RestController
+    static class ExampleRestController {
+
+        @GetMapping("/hello")
         String sayHello(@AuthenticationPrincipal OidcUser oidcUser) {
             return "Hello: " + oidcUser.getFullName();
         }
     }
-
 }
