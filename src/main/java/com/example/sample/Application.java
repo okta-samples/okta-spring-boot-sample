8810f37693d663624ec90af2975b1d3d5430b62c
Brian Demers <bdemers@apache.org>
2023-01-25 10:47:15 -0500

    Updates example to use Spring MVC + Thymeleaf (#60)

--- src/main/java/com/example/sample/Application.java
+++ src/main/java/com/example/sample/Application.java
@@ -1,25 +1,61 @@
 package com.example.sample;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.net.URI;
+import java.security.Principal;
+import java.util.Collections;
 
 @SpringBootApplication
+@EnableMethodSecurity(securedEnabled = true)
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @RestController
-    static class SimpleRestController {
+    /**
+     * This example controller has endpoints for displaying the user profile info on {code}/{code} and "you have been
+     * logged out page" on {code}/post-logout{code}.
+     */
+    @Controller
+    static class ExampleController {
+
         @GetMapping("/")
+        String home() {
+            return "home";
+        }
+
+        @GetMapping("/profile")
+        @PreAuthorize("hasAuthority('SCOPE_profile')")
+        ModelAndView userDetails(OAuth2AuthenticationToken authentication) {
+            return new ModelAndView("userProfile" , Collections.singletonMap("details", authentication.getPrincipal().getAttributes()));
+        }
+    }
+
+    @RestController
+    static class ExampleRestController {
+
+        @GetMapping("/hello")
         String sayHello(@AuthenticationPrincipal OidcUser oidcUser) {
             return "Hello: " + oidcUser.getFullName();
         }
     }
-
 }
---
+++ src/main/java/com/example/sample/SecurityConfiguration.java
@@ -0,0 +1,40 @@
+package com.example.sample;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
+    public SecurityConfiguration(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
+    OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler successHandler =
+            new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri("http://localhost:8080/");
+        return successHandler;
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+            // allow anonymous access to the root page
+            .requestMatchers("/").permitAll()
+            // all other requests
+            .anyRequest().authenticated());
+        http.logout((logout) -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler()));
+        // enable OAuth2/OIDC
+        http.oauth2Login();
+        http.oauth2ResourceServer().jwt();
+        
+        return http.build();
+    }
+}
