d54443c1ce5e27f85f80e4b314d83fb31a879b20
Matt Raible <matt@raibledesigns.com>
2023-03-07 10:03:53 -0700

    Add Thymeleaf and logout support

--- src/main/java/com/example/sample/Application.java
+++ src/main/java/com/example/sample/Application.java
@@ -2,10 +2,14 @@ package com.example.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.result.view.Rendering;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class Application {
@@ -14,12 +18,23 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @RestController
-    static class SimpleRestController {
+    /**
+     * This example controller has endpoints for displaying the user profile info on {code}/{code} and "you have been
+     * logged out page" on {code}/post-logout{code}.
+     */
+    @Controller
+    static class ExampleController {
+
         @GetMapping("/")
-        String sayHello(@AuthenticationPrincipal OidcUser oidcUser) {
-            return "Hello: " + oidcUser.getFullName();
+        String home() {
+            return "home";
+        }
+
+        @GetMapping("/profile")
+        @PreAuthorize("hasAuthority('SCOPE_profile')")
+        public Mono<Rendering> userDetails(OAuth2AuthenticationToken authentication) {
+            Map<String, Object> userAttributes = authentication.getPrincipal().getAttributes();
+            return Mono.just(Rendering.view("userProfile").modelAttribute("details", userAttributes).build());
         }
     }
-
 }
